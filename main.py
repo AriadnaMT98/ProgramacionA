66e0c1497f72744fbce541a88f103c532ad8ad94
Ariadna <aritelucha99@gmail.com>
2024-09-10 00:50:44 -0600

    Tarea 4

---
+++ main.py
@@ -0,0 +1,15 @@
+from coche import Coche
+
+#Instancia uno
+coche_uno = Coche("Suzuki","Swfit", 2022)
+coche_uno.mostrar_info()
+
+#Instancia dos
+print("********************************")
+coche_dos = Coche("Nissan","Civic", 2007)
+coche_dos.mostrar_info()
+
+#Instancia tres
+print("*********************************")
+coche_tres = Coche("Chevrolet", "Chevy", 2012)
+coche_tres.mostrar_info()
