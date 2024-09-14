47aed67ab22a3bc72cafc92bc7c394ce9c93761c
Ariadna <aritelucha99@gmail.com>
2024-09-13 22:56:45 -0600

    Tarea 6

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ curso1 = Curso("ProgramacionAv", 123, "Eder")
 curso2 = Curso("Mecanica", 456, "Calderon")
 curso3 = Curso("Mecanismos", 789, "José Gerardo")
 
-estudiante1 = Estudiante("Alexis", 20, 1)
-estudiante2 = Estudiante("Osvaldo", 24, 2)
+estudiante1 = Estudiante("Alexis", 23, 1)
+estudiante2 = Estudiante("Osvaldo", 26, 2)
 
 estudiante1.agregar_curso(curso1)
 estudiante2.agregar_curso(curso2)
@@ -32,22 +32,22 @@ while True:
     if opcion == 1:
         id_estudiante = int(input("Intrduce el ID de algun estudiante: "))
         print("---------------------------------------------")
-        estudiante_elegido = buscar_id(id_estudiante)
-        if estudiante_elegido:
-            estudiante_elegido.mostrar_info_estudiante()
+        estudiante_seleccionado = buscar_id(id_estudiante)
+        if estudiante_seleccionado:
+            estudiante_seleccionado.mostrar_info_estudiante()
         else:
             print("El estudiante no fue encontrado")
             
     elif opcion == 2:
         id_estudiante = int(input("Intrduce el ID de algun estudiante: "))
         print("---------------------------------------------")
-        estudiante_elegido = buscar_id(id_estudiante)
-        if estudiante_elegido:
+        estudiante_seleccionado = buscar_id(id_estudiante)
+        if estudiante_seleccionado:
             nombre_curso = input("Introduce el nombre del curso a agregar: ")
             instructor = input("Introduce el instructor del Curso: ")
             codigo_curso = int(input("Introduce el codigo del curso: "))
             nuevo_curso = Curso(nombre_curso, codigo_curso, instructor)
-            estudiante_elegido.agregar_curso(nuevo_curso)
+            estudiante_seleccionado.agregar_curso(nuevo_curso)
             
         else:
             print("El estudiante no fue localizado")
